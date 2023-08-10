8fc254ccb407aa674c8161d7f9b743764847c2f5
gustavo.ferreira <gantonio@bestprojects.com.br>
2023-08-10 09:38:52 -0300

    first

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ let keyCloak = new KeyCloakService(PERMISSIONS);
 
 let adminClient = new AdminClient({
     realm: 'CatiSementesV3',
-    serverUrl: 'http://localhost:8080',
+    serverUrl: 'http://10.153.18.52:8080',
     resource: 'CatiSementesFront',
     adminLogin: 'admin',
     adminPassword: 'admin'
